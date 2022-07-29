c041f67b632f104a2d926d0ebbd94253f3829c8c
nkirti28 <nalawade.kirti@gmail.com>
2022-07-29 07:59:20 -0500

    update idb.js

--- public/js/idb.js
+++ public/js/idb.js
@@ -49,5 +49,35 @@ function uploadBudget() {
   // get all records from store and set to a variable
   const getAll = budgetObjectStore.getAll();
 
-  // more to come...
+  // upon a successful .getAll() execution, run this function
+  getAll.onsuccess = function () {
+    // if there was data in indexedDb's store, let's send it to the api server
+    if (getAll.result.length > 0) {
+      fetch("/api/transaction", {
+        method: "POST",
+        body: JSON.stringify(getAll.result),
+        headers: {
+          Accept: "application/json, text/plain, */*",
+          "Content-Type": "application/json",
+        },
+      })
+        .then((response) => response.json())
+        .then((serverResponse) => {
+          if (serverResponse.message) {
+            throw new Error(serverResponse);
+          }
+          // open one more transaction
+          const transaction = db.transaction(["new_budget"], "readwrite");
+          // access the new_pizza object store
+          const budgetObjectStore = transaction.objectStore("new_budget");
+          // clear all items in your store
+          pizzaObjectStore.clear();
+
+          alert("All saved pizza has been submitted!");
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    }
+  };
 }
